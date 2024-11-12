d5be1661b405f4ef99cf730dc8317feb31eebd54
antoinegreuzard <antoin.greuzard@gmail.com>
2024-11-12 20:30:00 +0100

    feat: add a table

--- src/app/api/game/[roomId]/route.ts
+++ src/app/api/game/[roomId]/route.ts
@@ -1,5 +1,6 @@
 // src/app/api/game/[roomId]/route.ts
 import {NextRequest, NextResponse} from "next/server";
+import {query} from "@/utils/db";
 
 type GameState = {
   squares: ("X" | "O" | null)[];
@@ -9,22 +10,48 @@ type GameState = {
   winner?: string;
 };
 
-const gameStates: Record<string, GameState> = {};
-
-// GET: Récupérer l'état du jeu
+// GET: Récupérer l'état du jeu depuis la base de données
 export async function GET(req: NextRequest, {params}: { params: { roomId: string } }) {
   const {roomId} = params;
-  const gameState = gameStates[roomId] || {squares: Array(9).fill(null), isXNext: true};
-  return NextResponse.json(gameState);
+
+  const sql = "SELECT * FROM online_games WHERE room_id = $1";
+  const result = await query(sql, [roomId]);
+
+  if (result.rows.length > 0) {
+    const gameState = result.rows[0];
+    return NextResponse.json({
+      squares: JSON.parse(gameState.squares),
+      isXNext: gameState.isxnext,
+      playerName: gameState.player1_name,
+      opponentName: gameState.player2_name,
+      winner: gameState.winner,
+    });
+  }
+
+  // Initialiser l'état du jeu si la salle n'existe pas
+  const initialState: GameState = {squares: Array(9).fill(null), isXNext: true};
+  return NextResponse.json(initialState);
 }
 
-// POST: Mettre à jour l'état du jeu
+// POST: Mettre à jour l'état du jeu dans la base de données
 export async function POST(req: NextRequest, {params}: { params: { roomId: string } }) {
   const {roomId} = params;
-  const body = await req.json();
-  const {squares, isXNext, playerName, opponentName, winner} = body;
+  const {squares, isXNext, playerName, opponentName, winner} = await req.json();
 
-  // Mettre à jour l'état du jeu
-  gameStates[roomId] = {squares, isXNext, playerName, opponentName, winner};
+  const sql = `
+    INSERT INTO online_games (room_id, squares, isxnext, player1_name, player2_name, winner, updated_at)
+    VALUES ($1, $2, $3, $4, $5, $6, NOW()) ON CONFLICT (room_id)
+    DO
+    UPDATE SET
+      squares = EXCLUDED.squares,
+      isxnext = EXCLUDED.isxnext,
+      player1_name = EXCLUDED.player1_name,
+      player2_name = EXCLUDED.player2_name,
+      winner = EXCLUDED.winner,
+      updated_at = NOW();
+  `;
+
+  await query(sql, [roomId, JSON.stringify(squares), isXNext, playerName, opponentName, winner]);
+
   return NextResponse.json({message: "Game state updated"});
 }
